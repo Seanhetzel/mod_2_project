f334c9951a5c7826ff12241c1157f3fbe2867a7d
Larysa Cohee <larysa.cohee@flatironschool.com>
2019-07-31 14:24:02 -0700

    working on form

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   resources :quizzes
+  resources :user_options
+  resources :options
   root 'quizzes#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
