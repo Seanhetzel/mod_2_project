4c60741164156132bd40d59094a2f940b177423b
aminamos <26092352+aminamos@users.noreply.github.com>
2019-07-31 18:04:15 -0700

    draft

--- app/controllers/user_options_controller.rb
+++ app/controllers/user_options_controller.rb
@@ -5,12 +5,15 @@ class UserOptionsController < ApplicationController
         # params[:user_option][:option_response_2]
         # params[:user_option][:option_response_3]
         # UserOption.create(user_id: session[:user_id], quiz_id: params[:qid], option_response_1: params[:user_option][:option_response_1], option_response_2: params[:user_option][:option_response_2], option_response_3: params[:user_option][:option_response_3])
-        UserOption.create(user_id: session[:user_id], quiz_id: params[:qid], option_response_1: params[:user_option][:option_response_1], option_response_2: params[:user_option][:option_response_2], option_response_3: params[:user_option][:option_response_3])
+        @uo = UserOption.create(user_id: session[:user_id], quiz_id: params[:qid], option_response_1: params[:user_option][:option_response_1], option_response_2: params[:user_option][:option_response_2], option_response_3: params[:user_option][:option_response_3])
+        if @uo
+            @uo.save
+        end
     end
 
     private
 
     def result_params(*args)
-        
+        params.require(:user_option).permit(:option_response_1, :option_response_2, :option_response_3)
     end
 end
