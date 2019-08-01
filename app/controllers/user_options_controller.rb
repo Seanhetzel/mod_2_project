b414036bc25fb34b44534a9e78451ed05d7a5404
aminamos <26092352+aminamos@users.noreply.github.com>
2019-08-01 09:20:02 -0700

    almost MVP draft

--- app/controllers/user_options_controller.rb
+++ app/controllers/user_options_controller.rb
@@ -1,11 +1,7 @@
 class UserOptionsController < ApplicationController
     def create
         byebug
-        # params[:user_option][:option_response_1]
-        # params[:user_option][:option_response_2]
-        # params[:user_option][:option_response_3]
-        # UserOption.create(user_id: session[:user_id], quiz_id: params[:qid], option_response_1: params[:user_option][:option_response_1], option_response_2: params[:user_option][:option_response_2], option_response_3: params[:user_option][:option_response_3])
-        @uo = UserOption.create(user_id: session[:user_id], quiz_id: params[:qid], option_response_1: params[:user_option][:option_response_1], option_response_2: params[:user_option][:option_response_2], option_response_3: params[:user_option][:option_response_3])
+        @uo = UserOption.new(user_id: session[:user_id], quiz_id: params[:qid], option_response_1: params[:user_option][:option_response_1], option_response_2: params[:user_option][:option_response_2], option_response_3: params[:user_option][:option_response_3])
         if @uo
             @uo.save
         end
