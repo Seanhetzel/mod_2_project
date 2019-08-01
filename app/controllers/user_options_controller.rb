37309289cf6a5a593452022e57d2d04feebc090a
aminamos <26092352+aminamos@users.noreply.github.com>
2019-08-01 11:22:42 -0700

    Results page draft

--- app/controllers/user_options_controller.rb
+++ app/controllers/user_options_controller.rb
@@ -1,11 +1,20 @@
 class UserOptionsController < ApplicationController
     def create
-        byebug
         @uo = UserOption.new(user_id: session[:user_id], quiz_id: params[:qid], option_response_1: params[:user_option][:option_response_1], option_response_2: params[:user_option][:option_response_2], option_response_3: params[:user_option][:option_response_3])
         if @uo
             @uo.save
         end
-        
+        redirect_to user_options_results_path
+    end
+
+    def show
+
+    end
+
+    def results
+        @response_record = UserOption.find_by(user_id: session[:user_id])
+        @user = User.find(@response_record.user_id)
+        render '/quizzes/results'
     end
 
     private
